bcf9691435678efda8563d83476ee81d7552bb9f
Mackson Isani <isanimackson@gmail.com>
2022-06-17 09:16:01 +0100

    Modified database configuration

--- code/app.py
+++ code/app.py
@@ -10,7 +10,7 @@ from security import authenticate, identify
 from db import db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'A seeret key'
 
